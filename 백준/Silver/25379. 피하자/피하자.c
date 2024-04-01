563c50263186dc270dc465e4e3a1e4e2557148e1
Koakuma <117429962+Soakuma@users.noreply.github.com>
2024-04-01 20:29:18 +0900

    [Silver IV] Title: 피하자, Time: 240 ms, Memory: 5016 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/25379. 피하자/피하자.c
+++ 백준/Silver/25379. 피하자/피하자.c
@@ -1,52 +1,24 @@
 #include <stdio.h>
 
-int counter(int arr[], int n){
-  int count = 0;
-  for(int i = 0; i < n-1; i++){
-    if((arr[i] % 2 == 0 && arr[i+1] % 2 == 1) || (arr[i] % 2 == 1 && arr[i+1] % 2 == 0)){
-      count++;
-    }
-  }
-  return count;
-}
+int arr[1000000];
+
 int main(){
-  int n;
-  scanf("%d", &n);
-  int oddarr[n];
-  int evenarr[n];
-  int inp;
-  for(int i = 0; i < n; i++){
-    scanf("%d", &inp);
-    evenarr[i] = inp;
-    oddarr[i] = inp;
-  }
-  int odd_count = 0;
-  while(1){
-    for(int i = 0; i < n-1; i++){
-      if(oddarr[i] % 2 == 0 && oddarr[i+1] % 2 == 1){
-        int temp = oddarr[i];
-        oddarr[i] = oddarr[i+1];
-        oddarr[i+1] = temp;
-        odd_count++;
-      }
-    }
-    if(counter(oddarr, n) <= 1){
-      break;
-    }
-  }
-  int even_count = 0;
-  while(1){
-    for(int i = 0; i < n-1; i++){
-      if(evenarr[i] % 2 == 1 && evenarr[i+1] % 2 == 0){
-        int temp = evenarr[i];
-        evenarr[i] = evenarr[i+1];
-        evenarr[i+1] = temp;
-        even_count++;
-      }
-    }
-    if(counter(evenarr, n) <= 1){
-      break;
-    }
-  }
-  printf("%d", odd_count > even_count ? even_count : odd_count);
+	int n;
+	long long int even = 0, odd = 0, even_count = 0, odd_count = 0;
+	scanf("%d", &n);
+	for(int i = 0; i < n; i++){
+		scanf("%d", &arr[i]);
+		if(arr[i] % 2 == 0){
+			even++;
+			odd_count += odd;
+		}else{
+			odd++;
+			even_count += even;
+		}
+	}
+	if(even_count < odd_count){
+		printf("%lld", even_count);
+	}else{
+		printf("%lld", odd_count);
+	}
 }
