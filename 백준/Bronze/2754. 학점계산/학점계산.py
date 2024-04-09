04c44c515ea88258d9596a234daa0bf082134b2b
Koakuma <117429962+Soakuma@users.noreply.github.com>
2024-04-09 14:49:29 +0900

    [Bronze III] Title: 학점계산, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,28 @@
+sc = input()
+
+if sc == 'A+':
+    print(4.3)
+elif sc == 'A0':
+    print(4.0)
+elif sc == 'A-':
+    print(3.7)
+elif sc == 'B+':
+    print(3.3)
+elif sc == 'B0':
+    print(3.0)
+elif sc == 'B-':
+    print(2.7)
+elif sc == 'C+':
+    print(2.3)
+elif sc == 'C0':
+    print(2.0)
+elif sc == 'C-':
+    print(1.7)
+elif sc == 'D+':
+    print(1.3)
+elif sc == 'D0':
+    print(1.0)
+elif sc == 'D-':
+    print(0.7)
+elif sc == 'F':
+    print(0.0)
